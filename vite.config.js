9322b971a8019bd2f166a5cb50c1bdfc770614f0
IONHAX04 <thirukumara04@gmail.com>
2024-09-23 07:46:50 +0530

    Latest

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import commonjs from '@rollup/plugin-commonjs'
-import legacy from '@vitejs/plugin-legacy';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-    }),
-    commonjs() // This will transform CommonJS modules to ES modules
-  ],
-})
+  assetsInclude: ["**/*.JPG", "**/*.jpg"],
+  plugins: [react()],
+});
